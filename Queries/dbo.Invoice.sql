a96fefa12c91f7a2e4e6aad97bfa84581da7720a
Mark Brennan <brennan-m14@email.ulster.ac.uk>
2015-02-11 11:46:36 +0000

    Hotel Booking System
    Added partially working database registration process.

---
+++ Queries/dbo.Invoice.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Invoice] (
+    [InvoiceID]     INT IDENTITY (1, 1) NOT NULL,
+    [ReservationID] INT NULL,
+    [CustomerID]    INT NULL,
+    [InvoiceDate]   SMALLDATETIME   NULL,
+    PRIMARY KEY CLUSTERED ([InvoiceID] ASC)
+);
+
